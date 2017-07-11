932891105007a6a5ae9e7fdcc33bcb5e1cf56467
Ascariel <pablocangas@gmail.com>
2017-07-11 05:04:12 -0400

    fixing mailer produccion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.action_mailer.default_url_options = { :host => "sheltered-brushlands-64231.herokuapp.com" }
-
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true  
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
